42d467f1fb37656d08f44d31f14fe2fa1808caf3
Jose Erick Benitez Renteria <capitan_ayb@hotmail.com>
2020-07-09 22:14:32 -0600

    segundo intento

---
+++ js/funciones.js
@@ -0,0 +1,35 @@
+let hide = document.getElementsByClassName('hide').href = '../css/styles.css';
+let estilos = document.getElementById('show').href = '../css/styles.css';
+var cath = 0;
+
+function ocultar() {
+
+
+
+    $("#show").hide();
+    $("#mostrar").show();
+
+
+    console.log("uno");
+
+    console.log('Hola');
+}
+
+
+
+
+
+
+
+function mostrar() {
+
+    $("#show").show();
+
+
+    console.log("cero");
+
+
+
+    console.log('show');
+
+}
