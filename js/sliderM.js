c975d337938325b43a2c2dd1172f6b332ef4ff12
Jose Erick Benitez Renteria <capitan_ayb@hotmail.com>
2020-07-15 02:26:00 -0600

    parte siete

--- js/sliderM.js
+++ js/sliderM.js
@@ -1,8 +1,8 @@
 let slider = document.querySelector(".contenido-slider");
-let sliderIndividual = document.querySelectorAll(".item-sl");
+let sliderIndividual = document.querySelectorAll(".contenido-slider img");
 let contador = 1;
 let width = sliderIndividual[0].clientWidth;
-let intervalo = 3000;
+let intervalo = 2000;
 
 window.addEventListener("resize", function() {
     width = sliderIndividual[0].clientWidth;
@@ -17,11 +17,12 @@ function slides() {
     slider.style.transform = "translate(" + (-width * contador) + "px)";
     slider.style.transition = "transform .7s";
     contador++;
+    let largo = sliderIndividual.length;
 
-    if (contador == sliderIndividual.length) {
+    if (contador == largo - 4) {
         setTimeout(function() {
             slider.style.transform = "translate(0px)";
-            slider.style.transition = "transform .7s";
+            slider.style.transition = "transform 0s";
             contador = 1;
 
         }, 1500);
